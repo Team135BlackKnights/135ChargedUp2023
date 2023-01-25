1dc0e692eedc8ded0da542afd9dbb5a7d530a86e
runeismyname <r.bonnell005@gmail.com>
2023-01-25 16:10:36 -0500

    Added stuff from design meeting

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -13,4 +13,19 @@ public interface RobotMap {
             shiftSolenoid = 0,
             shiftSolenoid2 = 1;
     }
+
+    public interface Intake {
+        // Simple intake. Motors spin vertically opposite of each other.
+        public static final int
+            intakeMotorID1 = 0,
+            intakeMotorID2 = 0;
+    }
+
+    public interface Manipulator {
+        // Manipulator tilts and is on a lift. Limit switches to set to certain positions.
+        public static final int
+            liftMotorID1 = 0,
+            liftMotorID2 = 0,
+            tiltMotorID = 0;
+    }
 }
