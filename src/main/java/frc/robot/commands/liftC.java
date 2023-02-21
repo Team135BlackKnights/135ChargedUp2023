d5956cb4e7416aae8e8cfe2329d8bdf00ed79d6b
Gavin <robotics@phm.k12.in.us>
2023-02-21 18:09:12 -0500

    Update liftC.java

--- src/main/java/frc/robot/commands/liftC.java
+++ src/main/java/frc/robot/commands/liftC.java
@@ -7,7 +7,7 @@ import frc.robot.subsystems.liftS;
 
 public class liftC extends CommandBase{
     private final liftS lift;
-    double Desired, pos;
+    double Desired, pos, liftSpeed;
     public liftC(liftS subsystem) {
         lift=subsystem;
         addRequirements(subsystem);    
@@ -18,14 +18,16 @@ public class liftC extends CommandBase{
     PIDController pidController = new PIDController(.1, .1, 0);//placeholder vals
 
     public void execute(){
-        if (RobotContainer.controller2.getPOV() == 0){
+        liftSpeed = (RobotContainer.controller2.getLeftY()/2);
+        lift.extendedLift(liftSpeed, -liftSpeed);
+        /*if (RobotContainer.controller2.getPOV() == 0){
             lift.extendedLift(-.5, .5);
         }
         else if (RobotContainer.controller2.getPOV() == 180){
             lift.extendedLift(.3, -.3);
-        }
+        }*/
 
-        liftS.tilt.set(RobotContainer.controller2.getLeftY());
+        liftS.tilt.set(RobotContainer.controller2.getRightY());
         /*if (RobotContainer.controller2.getAButton()==true){
             liftS.tilt.set(0.5);
         }
